b4ece011124783043c71ce5fee835a36249102a6
Mike Cunneen <mike@cunneen.biz>
2015-04-16 13:53:33 +0800

    Bump version. Add AppCache.

--- package.js
+++ package.js
@@ -1,9 +1,10 @@
 Package.describe({
-  version: '0.1.2',
+  version: '0.1.11',
   summary: "Rig the basics for packmeteor"
 });
 
 Package.on_use(function (api) {
+  api.use('appcache', ['client','server']);
   api.use('webapp', 'server');
   api.use('reload', 'client');
   api.use('routepolicy', 'server');
